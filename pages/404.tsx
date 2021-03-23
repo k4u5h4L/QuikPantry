045d1b5ffd813e6c66ae6e37b36cbfa840a79d61
k4u5h4L <kaushal.v.bhat@gmail.com>
2021-03-23 20:38:38 +0530

    FIx error page

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,17 +5,17 @@ const Navbar = dynamic(() => import("@/components/Home/Navbar/Navbar"), {
     ssr: false,
 });
 // import Sidebar from "@/components/Home/Sidebar/Sidebar";
-import Banner from "@/components/NotFound/Banner/Banner";
 import Error from "@/components/NotFound/Error/Error";
 import Footer from "@/components/Home/Footer/Footer";
 import GoToTop from "@/components/GoToTop";
+import Banner from "@/components/Shop/Banner/Banner";
 
 export default function NotFound() {
     return (
         <>
             <Navbar />
             {/* <Sidebar /> */}
-            <Banner />
+            <Banner title="Page not found" path="error" />
             <Error />
             <Footer />
             <GoToTop />
