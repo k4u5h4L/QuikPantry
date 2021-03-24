1207a9bec3794697ab35cceb656a1b1da02c254c
k4u5h4L <kaushal.v.bhat@gmail.com>
2021-03-24 21:27:13 +0530

    Fix mongo passwordless auth bug

--- components/Product/Reviews/Reviews.tsx
+++ components/Product/Reviews/Reviews.tsx
@@ -1,8 +1,24 @@
-import { ReviewType } from "@/types/index";
 import React, { useState } from "react";
+import { useSession } from "next-auth/client";
+
+import { ReviewType } from "@/types/index";
+import Link from "next/link";
 
 export default function Reviews({ product }) {
+    const [session] = useSession();
+
     const [reviews, setReviews] = useState<boolean>(false);
+
+    const [title, setTitle] = useState<string>("");
+    const [desc, setDesc] = useState<string>("");
+
+    const titleChangeHandler = (e: any): void => {
+        setTitle(e.target.value);
+    };
+    const descChangeHandler = (e: any): void => {
+        setDesc(e.target.value);
+    };
+
     return (
         <div className="tab products-details-tab">
             <div className="row">
@@ -121,66 +137,82 @@ export default function Reviews({ product }) {
                                                 )
                                             )}
                                         </div>
-                                        <div className="review-form">
-                                            <h3>Write a Review</h3>
-                                            <form>
-                                                <div className="row">
-                                                    <div className="col-lg-6 col-md-6">
-                                                        <div className="form-group">
-                                                            <input
-                                                                type="text"
-                                                                id="name"
-                                                                name="name"
-                                                                placeholder="Enter your name"
-                                                                className="form-control"
-                                                            />
+
+                                        {session ? (
+                                            <div className="review-form">
+                                                <h3>Write a Review</h3>
+                                                <form>
+                                                    <div className="row">
+                                                        <div className="col-lg-6 col-md-6">
+                                                            <div className="form-group">
+                                                                <input
+                                                                    type="text"
+                                                                    id="name"
+                                                                    name="name"
+                                                                    placeholder="Enter your name"
+                                                                    className="form-control"
+                                                                />
+                                                            </div>
+                                                        </div>
+                                                        <div className="col-lg-6 col-md-6">
+                                                            <div className="form-group">
+                                                                <input
+                                                                    type="email"
+                                                                    id="email"
+                                                                    name="email"
+                                                                    placeholder="Enter your email"
+                                                                    className="form-control"
+                                                                />
+                                                            </div>
+                                                        </div>
+                                                        <div className="col-lg-12 col-md-12">
+                                                            <div className="form-group">
+                                                                <input
+                                                                    type="text"
+                                                                    id="review-title"
+                                                                    name="review-title"
+                                                                    placeholder="Enter your review a title"
+                                                                    className="form-control"
+                                                                />
+                                                            </div>
+                                                        </div>
+                                                        <div className="col-lg-12 col-md-12">
+                                                            <div className="form-group">
+                                                                <textarea
+                                                                    name="review-body"
+                                                                    id="review-body"
+                                                                    cols={30}
+                                                                    rows={7}
+                                                                    placeholder="Write your comments here"
+                                                                    className="form-control"
+                                                                ></textarea>
+                                                            </div>
+                                                        </div>
+                                                        <div className="col-lg-12 col-md-12">
+                                                            <button
+                                                                type="submit"
+                                                                className="default-btn"
+                                                            >
+                                                                Submit Review
+                                                            </button>
                                                         </div>
                                                     </div>
-                                                    <div className="col-lg-6 col-md-6">
-                                                        <div className="form-group">
-                                                            <input
-                                                                type="email"
-                                                                id="email"
-                                                                name="email"
-                                                                placeholder="Enter your email"
-                                                                className="form-control"
-                                                            />
-                                                        </div>
-                                                    </div>
-                                                    <div className="col-lg-12 col-md-12">
-                                                        <div className="form-group">
-                                                            <input
-                                                                type="text"
-                                                                id="review-title"
-                                                                name="review-title"
-                                                                placeholder="Enter your review a title"
-                                                                className="form-control"
-                                                            />
-                                                        </div>
-                                                    </div>
-                                                    <div className="col-lg-12 col-md-12">
-                                                        <div className="form-group">
-                                                            <textarea
-                                                                name="review-body"
-                                                                id="review-body"
-                                                                cols={30}
-                                                                rows={7}
-                                                                placeholder="Write your comments here"
-                                                                className="form-control"
-                                                            ></textarea>
-                                                        </div>
-                                                    </div>
-                                                    <div className="col-lg-12 col-md-12">
-                                                        <button
-                                                            type="submit"
-                                                            className="default-btn"
-                                                        >
-                                                            Submit Review
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                            </form>
-                                        </div>
+                                                </form>
+                                            </div>
+                                        ) : (
+                                            <div className="review-form">
+                                                <hr />
+                                                <h3>
+                                                    You can post comments only
+                                                    when you&apos;re logged in.
+                                                </h3>
+                                                <Link href="/login">
+                                                    <a className="default-btn">
+                                                        Login
+                                                    </a>
+                                                </Link>
+                                            </div>
+                                        )}
                                     </div>
                                 </div>
                             </div>
